d6750888e31fa5072b6408cfd3c35d328ce9e7c0
Mbusiseni Mthembu <mmthembu@e4.co.za>
2025-05-08 15:14:05 +0200

    Matter Details

--- Hackathon_Application_UI/Models/MatterVM.cs
+++ Hackathon_Application_UI/Models/MatterVM.cs
@@ -6,11 +6,15 @@
         {
             MatterfList = new List<Matter>();
             Matter = new Matter();
+            RejectedMatters = new List<Matter>();
+            CompletedMatters = new List<Matter>();
+            DocumentList = new List<Document>();
         }
         public Matter Matter { get; set; }
         public List<Matter> MatterfList { get; set; }
         public List<Matter> RejectedMatters { get; set; }
         public List<Matter> CompletedMatters { get; set; }
+        public List<Document> DocumentList { get; set; }
 
     }
 }
